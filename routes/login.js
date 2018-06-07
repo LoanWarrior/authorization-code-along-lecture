db2dd072abfcf47653f0b767f40a0e6c61036b5d
Edison <edisontoole@gmail.com>
2018-06-07 14:51:47 -0600

    poop'

--- routes/login.js
+++ routes/login.js
@@ -1,8 +1,36 @@
-const express = require('express');
-const router = express.Router();
+const express = require('express')
+const router = express.Router()
+const knex = require('../knex')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+require('dotenv').config()
 
-router.get('/',(req,res,next)=>{
-  res.render('login',{errorMessage:''})
+router.get('/', function(req, res, next) {
+  res.render('login', { errorMessage: '' })
 })
 
-module.exports = router;
+router.post('/', function (req, res, next) {
+  const username = req.body.username
+  const password = req.body.password  
+
+  if (username && password) {
+    knex('users')
+      .where('username', username)
+      .then((result) => {
+        if (result.length !== 1) {
+          res.status(400).send(JSON.stringify({ message: 'Bad username'}))
+        }
+        else if (bcrypt.compareSync(password, result[0].password)) {
+          res.status(400).send(JSON.stringify({ message: 'You HACK THE GIBSON'}))
+        }
+        else {
+          res.status(400).send(JSON.stringify({ message: 'Bad password'}))
+        }
+      })
+  }
+  else {
+    res.status(400).send(JSON.stringify({ message: 'Must username and password'}))
+  }
+})
+
+module.exports = router
